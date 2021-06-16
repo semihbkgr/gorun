a2f1fec995c4a598204a2b199a6470a7f566d4da
SemihBG <buyukgungorsemih@gmail.com>
2021-06-16 23:40:34 +0300

    Website project

---
+++ website/src/test/java/com/semihbg/gorun/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semihbg.gorun.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
