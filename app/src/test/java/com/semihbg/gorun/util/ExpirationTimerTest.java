a3597578acc2e5b6c2f95a1a0bf40cfbf1091e36
SemihBG <buyukgungorsemih@gmail.com>
2021-07-28 20:21:34 +0300

    Gradle text dependencies and configuration, ExpirationTimer api and test

---
+++ app/src/test/java/com/semihbg/gorun/util/ExpirationTimerTest.java
@@ -0,0 +1,44 @@
+package com.semihbg.gorun.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("ExpirationTimer")
+class ExpirationTimerTest {
+
+    @Test
+    @DisplayName("Get Result Before Expiration")
+    void getResultBeforeExpiration() throws InterruptedException {
+        ExpirationTimer expirationTimer=new ExpirationTimer(100L);
+        expirationTimer.reflesh();
+        Thread.sleep(50L);
+        assertTrue(expirationTimer.result());
+        expirationTimer.stop();
+    }
+
+    @Test
+    @DisplayName("Get Result After Expiration")
+    void getResultAfterExpiration() throws InterruptedException {
+        ExpirationTimer expirationTimer=new ExpirationTimer(50L);
+        expirationTimer.reflesh();
+        Thread.sleep(100L);
+        assertFalse(expirationTimer.result());
+        expirationTimer.stop();
+    }
+
+    @Test
+    @DisplayName("Get Result After Stop")
+    void getResultAfterStop() throws InterruptedException {
+        ExpirationTimer expirationTimer=new ExpirationTimer(100L);
+        expirationTimer.reflesh();
+        Thread.sleep(50L);
+        assertThrows(IllegalStateException.class,
+                ()->{
+                    expirationTimer.stop();
+                    expirationTimer.result();
+                });
+    }
+
+}
