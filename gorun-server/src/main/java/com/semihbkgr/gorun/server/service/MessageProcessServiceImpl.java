57417d452388f42f3169e77f84eca1b8037bd3a2
SemihBG <buyukgungorsemih@gmail.com>
2021-09-25 19:02:09 +0300

    MessageProcessServiceImpl input and interrupt command process and testes

--- gorun-server/src/main/java/com/semihbkgr/gorun/server/service/MessageProcessServiceImpl.java
+++ gorun-server/src/main/java/com/semihbkgr/gorun/server/service/MessageProcessServiceImpl.java
@@ -6,11 +6,12 @@ import com.semihbkgr.gorun.server.message.Message;
 import com.semihbkgr.gorun.server.run.RunStatus;
 import com.semihbkgr.gorun.server.socket.RunWebSocketSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.buffer.DataBufferUtils;
-import org.springframework.core.io.buffer.DefaultDataBufferFactory;
+import org.springframework.core.io.buffer.*;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
 
 @Service
@@ -25,6 +26,10 @@ public class MessageProcessServiceImpl implements MessageProcessService {
         switch (message.command) {
             case RUN:
                 return processRunCommand(session, message);
+            case INPUT:
+                return processInputCommand(session,message);
+            case INTERRUPT:
+                return processInterruptCommand(session, message);
             default:
                 return Flux.error(new IllegalStateException("Unhandled message command, command: " + message.command.name()));
         }
@@ -45,8 +50,30 @@ public class MessageProcessServiceImpl implements MessageProcessService {
                     .map(messageBody -> Message.of(Command.OUTPUT, messageBody))
                     .onErrorReturn(Message.of(Command.ERROR));
         } else
-            return Flux.just(Message.of(Command.ERROR, "This session already has an on going process"));
+            return Flux.just(Message.of(Command.ERROR, "This session has  already an on going process"));
     }
 
+    private Flux<Message> processInputCommand(RunWebSocketSession session, Message message) {
+        if (session.runContext != null && session.runContext.status() == RunStatus.EXECUTING) {
+            return DataBufferUtils.write(
+                    DataBufferUtils.readInputStream(
+                            ()->new ByteArrayInputStream(message.body.getBytes(StandardCharsets.UTF_8)),
+                            new DefaultDataBufferFactory(),256),
+                    session.runContext.process().getOutputStream()
+            )
+                    .thenMany(Mono.just(Message.of(Command.INFO)));
+        }else
+            return Flux.just(Message.of(Command.ERROR, "This session has not any on going process"));
+    }
+
+    private Flux<Message> processInterruptCommand(RunWebSocketSession session, Message message) {
+        if (session.runContext != null && session.runContext.status() == RunStatus.EXECUTING) {
+            return Flux.defer(() -> {
+                session.runContext.process().destroy();
+                return Mono.just(Message.of(Command.INFO));
+            });
+        } else
+            return Flux.just(Message.of(Command.ERROR, "This session has not any on going process"));
+    }
 
 }
--- gorun-server/src/test/java/com/semihbkgr/gorun/server/service/MessageProcessServiceImplTest.java
+++ gorun-server/src/test/java/com/semihbkgr/gorun/server/service/MessageProcessServiceImplTest.java
@@ -7,16 +7,15 @@ import com.semihbkgr.gorun.server.message.Message;
 import com.semihbkgr.gorun.server.socket.RunWebSocketSession;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import reactor.test.StepVerifier;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MessageProcessServiceImplTest {
 
-    static final String ROOT_DIR="test-files";
+    static final String ROOT_DIR = "test-files";
 
     MessageProcessServiceImpl messageProcessService;
     RunWebSocketSession session;
@@ -25,11 +24,11 @@ class MessageProcessServiceImplTest {
 
     @BeforeEach
     void initializeRequiredObjects() throws IOException {
-        this.session=new RunWebSocketSession();
-        this.fileService=new FileServiceImpl(ROOT_DIR);
+        this.session = new RunWebSocketSession();
+        this.fileService = new FileServiceImpl(ROOT_DIR);
         fileService.createRootDirIfNotExists();
-        this.fileNameGenerator=new SequentialFileNameGenerator();
-        this.messageProcessService=new MessageProcessServiceImpl(fileService,fileNameGenerator);
+        this.fileNameGenerator = new SequentialFileNameGenerator();
+        this.messageProcessService = new MessageProcessServiceImpl(fileService, fileNameGenerator);
     }
 
     @AfterEach
@@ -37,19 +36,48 @@ class MessageProcessServiceImplTest {
         fileService.clearAndDeleteRootDir();
     }
 
+
+
+    // Test methods below test only one message at a time.
+
     @Test
-    void commandRun(){
-        var messageBody="package main\n" +
+    void commandRun() {
+        var messageBody = "package main\n" +
                 "\n" +
                 "func main(){\n" +
                 "\tprint(\"HelloWorld\")\n" +
                 "}\n";
-        var session=new RunWebSocketSession();
-        var message= Message.of(Command.RUN,messageBody);
-        var messageFlux=messageProcessService.process(session,message);
+        var session = new RunWebSocketSession();
+        var message = Message.of(Command.RUN, messageBody);
+        var messageFlux = messageProcessService.process(session, message).log();
         StepVerifier.create(messageFlux)
-                .expectNext(Message.of(Command.OUTPUT,"HelloWorld"))
+                .expectNext(Message.of(Command.OUTPUT, "HelloWorld"))
                 .verifyComplete();
     }
+
+    @Test
+    @DisplayName("CommandInputWhenNoOnGoingProcess")
+    void commandInputWhenNoOnGoingProcess() {
+        var message = Message.of(Command.INPUT, "inputData");
+        var messageFlux = messageProcessService.process(session, message).log();
+        StepVerifier.create(messageFlux)
+                .expectNextMatches(responseMessage -> responseMessage.command == Command.ERROR)
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("CommandInterruptWhenNoOnGoingProcess")
+    void commandInterruptWhenNoOnGoingProcess() {
+        var message = Message.of(Command.INTERRUPT);
+        var messageFlux = messageProcessService.process(session, message).log();
+        StepVerifier.create(messageFlux)
+                .expectNextMatches(responseMessage -> responseMessage.command == Command.ERROR)
+                .verifyComplete();
+    }
+
+
+
+    // Test methods below test a sequence of messages at a time.
+
 
 }
