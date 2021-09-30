c681784fb09afa5933452157340cf192d174e492
SemihBG <buyukgungorsemih@gmail.com>
2021-09-30 20:21:14 +0300

    Server add versionId and order fields in snippet and SnippetInfo model, update snippets.json adn related test classes

--- gorun-server/src/main/java/com/semihbkgr/gorun/server/snippet/Snippet.java
+++ gorun-server/src/main/java/com/semihbkgr/gorun/server/snippet/Snippet.java
@@ -2,7 +2,6 @@ package com.semihbkgr.gorun.server.snippet;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
@@ -11,6 +10,8 @@ import lombok.NoArgsConstructor;
 public class Snippet {
 
     private int id;
+    private int versionId;
+    private int order;
     private String title;
     private String brief;
     private String explanation;
--- gorun-server/src/main/java/com/semihbkgr/gorun/server/snippet/SnippetInfo.java
+++ gorun-server/src/main/java/com/semihbkgr/gorun/server/snippet/SnippetInfo.java
@@ -1,20 +1,27 @@
 package com.semihbkgr.gorun.server.snippet;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder(access = AccessLevel.PRIVATE)
 public class SnippetInfo {
 
     private int id;
+    private int versionId;
+    private int order;
     private String title;
     private String brief;
 
     public static SnippetInfo of(Snippet snippet) {
-        return new SnippetInfo(snippet.getId(), snippet.getTitle(), snippet.getBrief());
+        return SnippetInfo.builder()
+                .id(snippet.getId())
+                .versionId(snippet.getVersionId())
+                .order(snippet.getOrder())
+                .title(snippet.getTitle())
+                .brief(snippet.getBrief())
+                .build();
     }
 
 }
